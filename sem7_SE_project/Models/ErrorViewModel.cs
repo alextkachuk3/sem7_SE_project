41a664318a7025e226ba0e532f30258e4b92d505
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-11-25 22:56:37 +0200

    Reconfigure project to MVC

---
+++ sem7_SE_project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sem7_SE_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
